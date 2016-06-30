97c7b70254402fb73d60e917693c452a6064b471
Artur Komarov <artur.komarov@gmail.com>
2016-06-30 17:04:27 +0300

    update feedback email

--- dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/Contact.java
+++ dspace-xmlui/src/main/java/org/dspace/app/xmlui/aspect/artifactbrowser/Contact.java
@@ -28,8 +28,8 @@ import org.xml.sax.SAXException;
 
 /**
  * Create a simple contact us page. Fancier contact us pages should be handled by the theme.
- * 
- * 
+ *
+ *
  * @author Scott Phillips
  */
 public class Contact extends AbstractDSpaceTransformer implements CacheableProcessingComponent
@@ -37,34 +37,34 @@ public class Contact extends AbstractDSpaceTransformer implements CacheableProce
     /** language strings */
     private static final Message T_title =
         message("xmlui.ArtifactBrowser.Contact.title");
-    
+
     private static final Message T_dspace_home =
         message("xmlui.general.dspace_home");
-    
-    private static final Message T_trail = 
+
+    private static final Message T_trail =
         message("xmlui.ArtifactBrowser.Contact.trail");
-    
-    private static final Message T_head = 
+
+    private static final Message T_head =
         message("xmlui.ArtifactBrowser.Contact.head");
-    
+
     private static final Message T_para1 =
         message("xmlui.ArtifactBrowser.Contact.para1");
 
     private static final Message T_feedback_label =
         message("xmlui.ArtifactBrowser.Contact.feedback_label");
-    
+
     private static final Message T_feedback_link =
         message("xmlui.ArtifactBrowser.Contact.feedback_link");
-    
-    private static final Message T_email = 
+
+    private static final Message T_email =
         message("xmlui.ArtifactBrowser.Contact.email");
-    
-    
+
+
     /**
      * Generate the unique caching key.
      * This key must be unique inside the space of this component.
      */
-    public Serializable getKey() 
+    public Serializable getKey()
     {
        return "1";
     }
@@ -72,40 +72,40 @@ public class Contact extends AbstractDSpaceTransformer implements CacheableProce
     /**
      * Generate the cache validity object.
      */
-    public SourceValidity getValidity() 
+    public SourceValidity getValidity()
     {
         return NOPValidity.SHARED_INSTANCE;
     }
-    
-    
+
+
     public void addPageMeta(PageMeta pageMeta) throws SAXException,
             WingException, UIException, SQLException, IOException,
             AuthorizeException
     {
         pageMeta.addMetadata("title").addContent(T_title);
-       
+
         pageMeta.addTrailLink(contextPath + "/",T_dspace_home);
         pageMeta.addTrail().addContent(T_trail);
     }
 
-  
+
     public void addBody(Body body) throws SAXException, WingException,
             UIException, SQLException, IOException, AuthorizeException
     {
         Division contact = body.addDivision("contact","primary");
-     
+
         contact.setHead(T_head);
-        
+
         String name = ConfigurationManager.getProperty("dspace.name");
         contact.addPara(T_para1.parameterize(name));
-        
+
         List list = contact.addList("contact");
-        
+
         list.addLabel(T_feedback_label);
         list.addItem().addXref(contextPath+"/feedback",T_feedback_link);
-        
+
         list.addLabel(T_email);
-        String email = ConfigurationManager.getProperty("feedback.recipient");
-        list.addItem().addXref("mailto:"+email,email); 
+        String email = "dspace@phystech.edu";
+        list.addItem().addXref("mailto:"+email,email);
     }
 }
